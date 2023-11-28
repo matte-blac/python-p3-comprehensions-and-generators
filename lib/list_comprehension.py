0c4c923cfc583d8602d64f6d6d626ba0090eb2bf
Matthew Bomett <bomettmat2@gmail.com>
2023-11-28 19:33:57 +0300

    Completed Assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    # use list comprehension to make a new list
+    return [num for num in num_list
+            if num % 2 == 0]
+    # returns a new list that consists all of even 
+    # integers of a sequence of integers 
+pass
 
 def make_exclamation(sentence_list):
-    pass
+    return [sentence + '!' for sentence in sentence_list]
+    # returns a new list that takes a list of sentence 
+    # strings with exclamation marks at the end
+pass
